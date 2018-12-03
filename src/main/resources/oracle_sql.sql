dee787e09fba8fdb6be72f0a3d6b4a5442489287
hoon <hoonest01@gmail.com>
2018-12-03 18:20:17 +0900

    sql

--- src/main/resources/oracle_sql.sql
+++ src/main/resources/oracle_sql.sql
@@ -7,6 +7,13 @@ grant create view to scott;
 conn scott/tiger;
 show user;
 
+create view emp_view30
+as
+select empno, ename, deptno from emp_copy
+where deptno = 30;
 
+select * from emp_view30;
+
+desc emp_view30
 
 
